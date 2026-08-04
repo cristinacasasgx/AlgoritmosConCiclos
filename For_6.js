e9d6a860928c7c3c0aefed9ccca469381b612065
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-08-04 18:21:53 -0500

    Ejercicio for 6 completado

---
+++ For_6.js
@@ -0,0 +1,9 @@
+// 6.	Recorrido de arreglos: Define un arreglo de nombres y utiliza un bucle para mostrar cada nombre individualmente en la pantalla.
+
+function arregloNombres() {
+    let nombres = ["Sofia", "Sara", "Emilia", "Matilde"];
+    for (let i = 0; i < nombres.length; i++) {
+        console.log(nombres[i]);
+    }
+}
+arregloNombres();
