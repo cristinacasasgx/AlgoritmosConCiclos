c460a0932374036ad8a80c5375027f8ca8926fa2
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-08-04 18:59:38 -0500

    Ejercicio While 1 completado

---
+++ While_1.js
@@ -0,0 +1,11 @@
+// 1.	Conteo Progresivo: Crea un programa que imprima en la consola los números del 1 al 100 utilizando una variable contador
+
+function conteoProgresivo() {
+    let contador = 1;
+
+    while (contador <= 100) {
+        console.log(contador);
+        contador++;
+    }
+}
+conteoProgresivo();
