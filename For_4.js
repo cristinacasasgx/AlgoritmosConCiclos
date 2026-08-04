87b70da8e186749565a0312386a35c8bc3148ea4
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-08-04 18:18:17 -0500

    Ejercicio for 4 completado

---
+++ For_4.js
@@ -0,0 +1,8 @@
+// 4.	Cuenta regresiva: Utiliza una variable de control para realizar un conteo hacia atrás desde el número 10 hasta el 1.
+
+function conteoRegresivo() {
+    for (let i = 10; i >= 1; i--) {
+        console.log(i);
+    }
+}
+conteoRegresivo();
