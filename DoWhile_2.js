34903741c346a381918324d1ae1f4889011332c7
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-08-04 18:39:33 -0500

    Ejercicio DoWhile 2 completado

---
+++ DoWhile_2.js
@@ -0,0 +1,12 @@
+//2. Conteo ascendente: Utiliza un ciclo para imprimir los números del 1 al 100 en la consola, asegurándote de incrementar la variable en cada vuelta
+
+function conteoAscendente() {
+    let i = 1;
+
+    do {
+        console.log(i);
+        i++;
+    } while (i <= 100);
+}
+conteoAscendente();
+
