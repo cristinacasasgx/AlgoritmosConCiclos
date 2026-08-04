071725ab4b25896d03b1b198436566b2b6b2a580
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-08-04 18:16:56 -0500

    Ejercicio for 3 completado

---
+++ For_3.js
@@ -0,0 +1,8 @@
+// 3.	Incremento personalizado: Escribe un ciclo que cuente de 2 en 2, comenzando en 0 y terminando en 20.
+
+function conteo2en2() {
+    for (let i = 0; i <= 20; i += 2) {
+        console.log(i);
+    }
+}
+conteo2en2();
