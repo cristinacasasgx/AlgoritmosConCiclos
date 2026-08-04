babedeefb3aea1c95a436dca0fbb94d50fdffd31
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-08-04 18:13:45 -0500

    Ejercicio for 1 completado

---
+++ For_1.js
@@ -0,0 +1,8 @@
+// Conteo estándar: Crea un bucle que imprima en la consola los números del 1 al 100 de forma consecutiva.
+
+function conteo100() {
+    for (let i = 1; i <= 100; i++) {
+        console.log(i);
+    }
+}
+conteo100();
