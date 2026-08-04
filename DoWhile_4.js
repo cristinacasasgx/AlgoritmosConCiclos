eea5ec331588e48bee923098a0496d95d647f7ee
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-08-04 18:42:13 -0500

    Ejercicio DoWhile 4 completado

---
+++ DoWhile_4.js
@@ -0,0 +1,11 @@
+// 4. Cuenta regresiva: Realiza un conteo hacia atrás desde el número 15 hasta el 0 utilizando el decremento de la variable de control
+
+function cuentaRegresiva() {
+    let i = 15;
+
+    do {
+        console.log(i);
+        i--;
+    } while (i >= 0);
+}
+cuentaRegresiva();
