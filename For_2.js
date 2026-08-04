341ac0887a70e25527d5734eefd00b193ddf1b73
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-08-04 18:15:28 -0500

    Ejercicio for 2 completado

---
+++ For_2.js
@@ -0,0 +1,8 @@
+// 2.	Llenado de contenedor: Simula el llenado de un contenedor de manzanas imprimiendo el número de manzana actual desde la 1 hasta la 1000.
+
+function contenedorManzanas() {
+    for (let i = 1; i <= 1000; i++) {
+        console.log("Manzana #" + i);
+    }
+}
+contenedorManzanas ();
