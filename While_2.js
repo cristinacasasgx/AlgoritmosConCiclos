92c6c520382c92a6c5483281ba534420e2db1da2
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-08-04 19:03:01 -0500

    Ejercicio While 2 completado

---
+++ While_2.js
@@ -0,0 +1,11 @@
+//2. El Contenedor de Manzanas: Simula el llenado de un contenedor. Debes imprimir "Manzana número X" desde la 1 hasta llegar a la 1000
+
+function contenedorManzanas() {
+    let manzana = 1;
+
+    while (manzana <= 1000) {
+        console.log("Manzana número " + manzana);
+        manzana++;
+    }
+}
+contenedorManzanas();
