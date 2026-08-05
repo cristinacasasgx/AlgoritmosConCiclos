cff8b1de0dc08094b380ec705d8ae5314d9f6b30
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-08-04 19:13:50 -0500

    Ejercicio While 9 completado

---
+++ While_9.js
@@ -0,0 +1,11 @@
+//9. Detección de Bucle Infinito: Identifica y corrige un código donde la condición x <= 5 sea siempre verdadera porque falta el incremento de la variable, evitando así que el programa se bloquee
+
+function correccionBucleInfinito() {
+    let x = 1;
+
+    while (x <= 5) {
+        console.log(x);
+        x++; // Acá está la corrección que evita el bucle infinito
+    }
+}
+correccionBucleInfinito();
