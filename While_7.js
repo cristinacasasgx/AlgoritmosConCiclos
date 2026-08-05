708694cdbc7c521e309d9e3ecea5286b36409217
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-08-04 19:10:11 -0500

    Ejercicio While 7 completado

---
+++ While_7.js
@@ -0,0 +1,16 @@
+//7. Uso de Break: Escribe un ciclo que cuente del 1 al 10, pero que se interrumpa y salga del bucle inmediatamente cuando el contador llegue a 7
+
+function breakConWhile() {
+    let contador = 1;
+
+    while (contador <= 10) {
+
+        if (contador === 7) {
+            break;
+        }
+
+        console.log(contador);
+        contador++;
+    }
+}
+breakConWhile();
