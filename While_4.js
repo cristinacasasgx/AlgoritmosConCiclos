dfccca324d1faf8e99aafd6f38b02b714618c3e1
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-08-04 19:05:46 -0500

    Ejercicio While 4 completado

---
+++ While_4.js
@@ -0,0 +1,11 @@
+//4. Cuenta Regresiva: Utiliza un bucle para realizar un conteo hacia atrás desde el número 10 hasta el 1
+
+function cuentaRegresivaWhile() {
+    let contador = 10;
+
+    while (contador >= 1) {
+        console.log(contador);
+        contador--;
+    }
+}
+cuentaRegresivaWhile();
